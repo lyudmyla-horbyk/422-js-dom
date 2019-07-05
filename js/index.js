606e31e3b58359c56d8add915171661885901f12
Liudmyla Liashuk <liudmyla@Inwindity-MacBook-Pro.local>
2019-07-05 20:26:12 +0300

    culculator

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const link = document.querySelector('a');
+link.textContent = 'Mozilla Developer Network';
+
+
+const sect = document.querySelector('section');
+const para = document.createElement('p');
+para.textContent = 'We hope you enjoyed the ride.';
+sect.appendChild(para);
+
+const text = document.createTextNode(' — the premier source for web development knowledge.');
+const linkPara = document.querySelector('p');
+linkPara.appendChild(text);
+sect.appendChild(linkPara)
+
+linkPara.parentNode.removeChild(linkPara);
+
+para.style.color = 'white';
+para.style.backgroundColor = 'black';
+para.style.padding = '10px';
+para.style.width = '250px';
+para.style.textAlign = 'center';
+
+para.classList.add('chosen');
+
+const colorDisplay = document.querySelector('.clock');
+
+
+
